b6f16df12875b62ead32a0bc82d233037817a243
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-12 14:50:45 -0600

    index.js complete

--- jenn/src/index.js
+++ jenn/src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import {reducer} from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, (applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 
